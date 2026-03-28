8189ddaf2a81c4fec84228073254058d06b27089
Xiao <rubiesvelt@gmail.com>
2026-03-28 23:49:03 +1030

    Commit & Push

--- LRUCache.cpp
+++ LRUCache.cpp
@@ -14,11 +14,33 @@ private:
 
 public:
     LRUCache(int capacity) {
+        this->capacity = capacity;
     }
 
     int get(int key) {
+        if (mp.find(key) == mp.end()) {
+            return -1;
+        }
+        auto it = mp[key];
+        int value = it->second;  // this is when an iterator acts like a pointer
+        cache.erase(it);
+        cache.push_front({key, value});
+        mp[key] = cache.begin();
+        return value;
     }
 
     void put(int key, int value) {
+        if (mp.find(key) != mp.end()) {
+            auto it = mp[key];
+            cache.erase(it);
+            mp.erase(key);
+        }
+        cache.push_front({key, value});
+        mp[key] = cache.begin();
+        if (cache.size() > capacity) {
+            auto back = cache.back();
+            mp.erase(back.first);
+            cache.pop_back();
+        }
     }
 };
